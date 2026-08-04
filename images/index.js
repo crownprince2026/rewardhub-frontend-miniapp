64675c4c6c97963bdab44f6c92617d340de2f826
crownprince2026 <dennisfortportal@gmail.com>
2026-08-04 08:54:56 +0200

    Restore all Mini App images

---
+++ images/index.js
@@ -0,0 +1,124 @@
+/* =====================================================
+   CROWN PRINCE REWARD HUB
+   IMAGE ASSET MANIFEST
+
+   Centralized Image Registry
+   Version: 1.0.0
+===================================================== */
+
+/* =====================================================
+   BRANDING
+===================================================== */
+
+import Logo from "./branding/logo.png";
+import SplashLogo from "./branding/splash-logo.png";
+import TelegramProfile from "./branding/telegram-profile.png";
+import MiniAppIcon from "./branding/miniapp-icon.png";
+import Favicon from "./branding/favicon.png";
+import AndroidAppIcon from "./branding/android-app-icon.png";
+import BrandGuidelines from "./branding/brand-guidelines.png";
+
+/* =====================================================
+   LOADING
+===================================================== */
+
+import LoadingAnimation from "./loading/loading-animation.png";
+
+/* =====================================================
+   SOCIAL MEDIA
+===================================================== */
+
+import XCover from "./social/x-cover.png";
+import XPostTemplate from "./social/x-post-template.png";
+import FacebookCover from "./social/facebook-cover.png";
+import TelegramBanner from "./social/telegram-banner.png";
+import TelegramPostTemplate from "./social/telegram-post-template.png";
+import InstagramPost from "./social/instagram-post.png";
+import PromotionalBanner from "./social/promotional-banner.png";
+
+/* =====================================================
+   BACKGROUNDS
+===================================================== */
+
+import DashboardBackground from "./backgrounds/dashboard-bg.png";
+import AuthBackground from "./backgrounds/auth-bg.png";
+import RewardsBackground from "./backgrounds/rewards-bg.png";
+import WalletBackground from "./backgrounds/wallet-bg.png";
+
+/* =====================================================
+   ILLUSTRATIONS
+===================================================== */
+
+import EmptyState from "./illustrations/empty-state.png";
+import Maintenance from "./illustrations/maintenance.png";
+import NoData from "./illustrations/no-data.png";
+import Offline from "./illustrations/offline.png";
+import Success from "./illustrations/success.png";
+
+/* =====================================================
+   ICONS
+===================================================== */
+
+import CrownIcon from "./icons/crown.png";
+import CoinIcon from "./icons/coin.png";
+import RewardIcon from "./icons/reward.png";
+import WalletIcon from "./icons/wallet.png";
+import WithdrawIcon from "./icons/withdraw.png";
+import ReferralIcon from "./icons/referral.png";
+import TaskIcon from "./icons/task.png";
+import LeaderboardIcon from "./icons/leaderboard.png";
+import SupportIcon from "./icons/support.png";
+import HistoryIcon from "./icons/history.png";
+import SettingsIcon from "./icons/settings.png";
+import NotificationIcon from "./icons/notification.png";
+
+/* =====================================================
+   EXPORT
+===================================================== */
+
+/* =====================================================
+   REWARD HUB MINI APP
+   CENTRAL IMAGE MANIFEST
+
+   Single source of truth for all image assets.
+===================================================== */
+
+const Images = {
+
+    branding: {
+
+        logo: require("./branding/official-logo.png"),
+
+        splash: require("./branding/splash-logo.png"),
+
+        telegramProfile: require("./branding/telegram-profile.png"),
+
+        miniAppIcon: require("./branding/miniapp-icon.png"),
+
+        favicon: require("./branding/favicon.png"),
+
+        androidAppIcon: require("./branding/android-app-icon.png"),
+
+        brandGuidelines: require("./branding/branding-guidelines.png")
+
+    },
+
+    loading: {
+
+        animation: require("./loading/loading-animation.png")
+
+    },
+
+    social: {
+
+        kit: require("./social/social-kit.png")
+
+    }
+
+};
+
+export default Images;
+
+/* =====================================================
+   END OF IMAGE ASSET MANIFEST
+===================================================== */
